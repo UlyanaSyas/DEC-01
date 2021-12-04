ee2fce5bbd1919687ce1d67a384f246dbba2f00e
UlyanaSyas <95490419+UlyanaSyas@users.noreply.github.com>
2021-12-04 21:38:26 +0300

    Add files via upload

---
+++ lesson/lesson5.sql
@@ -0,0 +1,205 @@
+ALTER SESSION SET
+    nls_date_format='YYYY-MM-DD';
+
+-- 1. Нужно указать имя тех, кто зарабатывает более 12000 в год.
+
+SELECT
+    e.first_name,
+    e.last_name,
+    e.salary
+FROM
+    hr.employees e
+WHERE
+    e.salary > 12000;
+    
+-- 2. Ищем сотрудника номер 176, по коням, нам нужны фамилия и отдел.
+
+SELECT
+    e.employee_id,
+    e.last_name,
+    e.department_id
+FROM
+    hr.employees e
+WHERE
+    e.employee_id = '176';
+    
+-- 3. Ищем сотрудников с такой ЗП, которая вне диапазона от 5000 до 12000.
+
+SELECT
+    e.first_name,
+    e.salary
+FROM 
+    hr.employees e
+WHERE
+    e.salary NOT BETWEEN 5000 AND 12000;
+    
+-- Или же:
+
+SELECT
+    e.first_name,
+    e.salary
+FROM 
+    hr.employees e
+WHERE
+    e.salary < 5000
+OR
+    e.salary > 12000;
+
+-- 4. Нужно выбрать фамилию, должность, и дату найма на работу для Матоса и Тейлора. Запрос в возрастающем порядке даты найма.
+
+SELECT
+    e.last_name,
+    e.job_id,
+    e.hire_date
+FROM
+    hr.employees e
+WHERE
+    e.last_name in (
+    'Taylor',
+    'Matos'
+    )
+ORDER BY
+    e.hire_date asc;
+    
+-- 5. Отображение фамилий и departmen ID всех сотрудников 20 и 50 отделов в возрастающем алфавитном порядке имён.
+
+SELECT
+    e.last_name,
+    e.department_id
+FROM
+    hr.employees e
+WHERE
+    e.department_id in (
+    20,
+    50)
+ORDER BY
+    e.first_name desc;
+
+/*
+   6. Изменить запрос из 3-й задачи, перечислить фамилии и ЗП сотрудников, которые зарабатывают от
+   5000 до 12000 и находятся в отделе 20 или 50, обозначить колонки "Employee" & "Monthly salary".
+   Ограничинть набор для первых пяти полей.
+*/  
+
+SELECT
+    e.first_name as "Employee",
+    e.salary as "Monthly salary"
+FROM 
+    hr.employees e
+WHERE
+    e.salary >= 5000
+OR
+    e.salary <= 12000
+AND
+    e.department_id = 50
+AND Rownum <=5;
+    
+    
+-- 7. Запрос на отображение и дату приема на работу сотрудников, принятых на работу в 2006 году.
+
+SELECT
+    e.*
+FROM
+    hr.employees e
+WHERE
+    e.hire_date between 
+    ('2006-01-01')
+    and('2006-12-31');
+    
+
+-- 8. Запрос на отображение всех сотрудников, у которых нет руководителя.
+
+SELECT
+    e.*
+FROM
+    hr.employees e
+WHERE
+   e.manager_id is null;
+
+/* 
+   9. Запрос на отображение фамилий, ЗП и комиссии для всех сотрудников. Сортировка данных по ЗП и комисси в порядке убывания.
+   Отсортировывать нумерацию в ORDER BY.
+*/
+
+SELECT
+    e.last_name,
+    e.salary,
+    e.commission_pct
+FROM
+    hr.employees e
+ORDER BY
+    e.salary, e.commission_pct desc;
+
+-- 10. Запрос, на отображение фамилии и ЗП, зарабатывающих больше суммы, указанной после запроса.
+
+SELECT
+    e.last_name,
+    e.salary
+FROM
+    hr.employees e
+WHERE
+    e.salary > &salary;
+    
+-- 11. Фамилии сотрудников, у которых в фамилии третья буква "а".
+
+SELECT
+    e.last_name
+FROM
+    hr.employees e
+WHERE
+    last_name LIKE '__a%';
+
+-- 12. Фамилии сотрудников, у которых в фамилии есть буквы "а" и "е".
+
+SELECT
+    e.last_name
+FROM
+    hr.employees e
+WHERE
+    e.last_name LIKE '%a%' 
+    AND  e.last_name LIKE '%e%';
+    
+/*
+   13. Отображение фамилии, ЗП, должности всех сотрудников, кто в продажах (sales representative, stock clerk),
+   и чья ЗП не равна 2500, 3500, 7000.
+*/
+
+SELECT
+    e.last_name,
+    e.salary,
+    j.job_title
+FROM
+    hr.employees e,
+    hr.jobs j
+WHERE
+    e.job_id = j.job_id 
+    AND j.job_title IN ('Sales Кepresentative', 'Stock Сlerk')
+    AND e.salary NOT IN (2500, 3500, 7000);
+
+-- 14. Изменить 6-й запрос, что бы была фамилия, ЗП, комиссия всех сотрудников, у которых комиссия 20% и ограничить на 5 полей.
+
+SELECT
+    e.first_name as "Employee",
+    e.salary as "Monthly salary",
+    e.commission_pct
+FROM 
+    hr.employees e
+WHERE
+    e.commission_pct = 0.2 
+AND Rownum <=5;
+
+/*
+   15. Отобразить имя и фамилию с использованием шаблона (Last name, First name), назвaния отдела и ЗП для каждого сотрудника,
+   у которого ЗП больше ста косарей в год.
+*/
+
+SELECT
+    e.first_name as "First name",
+    e.last_name as "Last name",
+    d.department_name,
+    e.salary
+FROM
+    hr.employees e,
+    hr.departments d
+WHERE
+    e.salary*12 > 100000;
